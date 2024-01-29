0a8908be0018ef46dd35288eae002018559b8e4f
Chanikya K <68418067+chanikya1998@users.noreply.github.com>
2024-01-29 11:38:25 -0600

    Update 1_☎️_User_Data_Dashboard.py

--- pages/1_☎️_User_Data_Dashboard.py
+++ pages/1_☎️_User_Data_Dashboard.py
@@ -146,8 +146,8 @@ def main():
                                                     (df['From'].apply(lambda x: re.sub(r'\D', '', x)) == from_number[-10:]) &
                                                     (row['Start Time (local)'] + pd.Timedelta(hours=1) <= df['Start Time (local)']) &
                                                     (df['Start Time (local)'] <= row['Start Time (local)'] + pd.Timedelta(hours=10))]
-                    
-                    
+
+
                     if not call_back_row_nurse.empty:
                         # Ensure that the nurse called back and it's an outbound call
                         if call_back_row_nurse['Direction'].values[0] == 'OUTBOUND':
@@ -204,91 +204,18 @@ def main():
         except Exception as e:
             return f"Error: {e}"
 
-    # Function to check if a nurse met the criteria for an inbound call
-    def evaluate_performance_x(row):
-        try:
-            from_component, to_component = extract_call_components(row)
-            if row['Direction'] == 'INBOUND':
-                answer_time_str = row['Answer Time (local)']
-
-                # Check if answer time is '--:--:--', treat as no answer
-                if answer_time_str == '--:--:--':
-                    duration = 0
-                    from_number = re.sub(r'\D', '', row['From'])  # Extract only numeric part
-                    to_number = re.sub(r'\D', '', row['To'])      # Extract only numeric part
-
-                    # Check if the nurse answered the patient's call for at least 30 seconds
-                    if row['Duration'] >= 30:
-                        return f'Met criteria. Did not answer. Nurse called back at {row["Start Time (local)"]} to {from_number}. Answered for {row["Duration"]} seconds.'
-
-                    # Look for an outbound call initiated by the nurse to the patient
-                    call_back_row_nurse_in_x_time = df[(df['Direction'] == 'OUTBOUND') & 
-                                       (df['To'].apply(lambda x: re.sub(r'\D', '', x)[-10:]) == from_number[-10:]) & 
-                                       (df['From'].apply(lambda x: re.sub(r'\D', '', x)) == to_number) & 
-                                       (row['Start Time (local)'] - pd.Timedelta(seconds=10) <= df['Start Time (local)']) & 
-                                       (df['Start Time (local)'] <= row['Start Time (local)'] + pd.Timedelta(seconds=x)) & 
-                                       (df['Duration'] >= 1)].head(1)
-                
-                    # Look for an inbound call initiated by the patient to the nurse
-                    call_back_row_patient_in_x_time = df[(df['Direction'] == 'INBOUND') & 
-                                       (df['To'].apply(lambda x: re.sub(r'\D', '', x)[-10:]) == to_number) & 
-                                       (df['From'].apply(lambda x: re.sub(r'\D', '', x)) == from_number[-10:]) & 
-                                       (row['Start Time (local)'] - pd.Timedelta(seconds=10) <= df['Start Time (local)']) & 
-                                       (df['Start Time (local)'] <= row['Start Time (local)'] + pd.Timedelta(seconds=x)) & 
-                                       (df['Duration'] >= 30)].head(1)
-                    
-                    if not call_back_row_nurse_in_x_time.empty:
-                        # Ensure that the nurse called back and it's an outbound call
-                        if call_back_row_nurse_in_x_time['Direction'].values[0] == 'OUTBOUND':
-                            call_back_time_in_x_time = pd.to_datetime(call_back_row_nurse_in_x_time['Start Time (local)'].values[0]).tz_localize(None)
-                            duration_call_back_in_x_time = call_back_row_nurse_in_x_time['Duration'].values[0]
-                            return f'Met criteria. Did not answer. Nurse called back in',selected_duration_threshold,' seconds.'
-                        else:
-                            return f'Did not answer. Nurse did not call back in',selected_duration_threshold,' seconds.'
-                    elif not call_back_row_patient_in_x_time.empty:
-                        # Ensure that the patient called back and it's an inbound call
-                        if call_back_row_patient_in_x_time['Direction'].values[0] == 'INBOUND':
-                            call_back_time_in_x_time = pd.to_datetime(call_back_row_patient_in_x_time['Start Time (local)'].values[0]).tz_localize(None)
-                            duration_call_back_in_x_time = call_back_row_patient_in_x_time['Duration'].values[0]
-                            return f'Met criteria. Did not answer. Patient called back in',selected_duration_threshold,' seconds.'
-                        else:
-                            return f'Did not answer. Nurse did not call back in',selected_duration_threshold,' seconds.'
-                    else:
-                        return f'Did not answer. Nurse did not call back in 10hr.'
-                
-                answer_time = pd.to_datetime(answer_time_str).tz_localize(None)
-                end_time = pd.to_datetime(row['End Time (local)']).tz_localize(None)
-                duration = row['Duration']
-
-                # Check if the call was answered for at least 30 seconds
-                if duration >= 30:
-                    return f'Met criteria. Answered for {duration} seconds.'
-
-                else:
-                    return f"Did not meet criteria. Received call at {answer_time} from {row['From']}."
-
-            elif row['Direction'] == 'OUTBOUND':
-                # You can add additional conditions for outbound calls if needed
-                return 'Outbound call'
-
-            else:
-                return 'Not an inbound or outbound call'
-
-        except Exception as e:
-            return f"Error: {e}"
-
     # Streamlit App
     st.title('Nurse Phone Call Performance')
 
     # Convert 'Start Time (local)' column to datetime format
     df['Start Time (local)'] = pd.to_datetime(df['Start Time (local)'])
 
+    # Sidebar for selecting performance criteria and date range
+    performance_criteria = st.sidebar.selectbox('Select Performance Criteria', ['All', 'Answered', 'Missed','No Follow-up','Follow-up >1hr and <10hr','Follow-up <1hr'])
+
     # Allow selection of date range
     selected_from_date = st.sidebar.date_input('Select From Date', min_value=df['Start Time (local)'].min().date(), max_value=df['Start Time (local)'].max().date(), value=df['Start Time (local)'].min().date())
     selected_to_date = st.sidebar.date_input('Select To Date', min_value=df['Start Time (local)'].min().date(), max_value=df['Start Time (local)'].max().date(), value=df['Start Time (local)'].max().date())
-
-    # Add a slider for duration threshold (x)
-    selected_duration_threshold = st.sidebar.slider('Select Duration Threshold', min_value=1, max_value=2592000, value=360)  # You can adjust the min, max, and initial value accordingly
 
     # Check if at least one date is selected
     if not selected_from_date or not selected_to_date:
@@ -387,88 +314,6 @@ def main():
             # Display only specific columns
             display_columns = ['From', 'To', 'Start Time (local)', 'Duration', 'Direction', 'Performance']
             st.write(filtered_df[filtered_df['Performance'].str.startswith('Met criteria')][display_columns])
-
-    
-    # Sidebar for selecting performance criteria and date range
-    performance_criteria = st.sidebar.selectbox('Select Performance Criteria', ['All', 'Answered', 'Missed','No Follow-up','Follow-up >1hr and <10hr','Follow-up <1hr'])
-
-    # Check if at least one date is selected
-    if not selected_from_date or not selected_to_date:
-        st.warning("Please select both 'From' and 'To' dates.")
-    else:
-        # Filter DataFrame based on selected date range
-        filtered_df_x = df[(df['Start Time (local)'].dt.date >= selected_from_date) & (df['Start Time (local)'].dt.date <= selected_to_date)]
-
-        # Apply the function to the filtered DataFrame
-        filtered_df_x['Performance'] = filtered_df_x.apply(evaluate_performance_x, axis=1)
-
-        # Count the number of calls that meet and do not meet the criteria
-        met_criteria_count_x = len(filtered_df_x[filtered_df_x['Performance'].str.startswith('Met')])
-        Answer_Call_count_x = len(filtered_df_x[filtered_df_x['Performance'].str.startswith('Met criteria. Answered')])
-        Missed_Call_count_x = len(filtered_df_x[filtered_df_x['Performance'].str.startswith(('Met criteria. Did not answer.', 'Did not'))])
-        Nurse_Call_Back_count_x = len(filtered_df_x[filtered_df_x['Performance'].str.startswith('Met criteria. Did not answer. Nurse called back')])
-        Patient_Call_Back_count_x = len(filtered_df_x[filtered_df_x['Performance'].str.startswith('Met criteria. Did not answer. Patient called back')])
-        did_not_meet_criteria_count_x = len(filtered_df_x[filtered_df_x['Performance'].str.startswith('Did not')])
-        
-        # Count the number of inbound and outbound calls
-        inbound_call_count_x = len(filtered_df_x[filtered_df_x['Direction'] == 'INBOUND'])
-        outbound_call_count_x = len(filtered_df_x[filtered_df_x['Direction'] == 'OUTBOUND'])
-
-        # Display the counts on top of the dashboard
-        st.markdown(f"<p style='color:#5cb7a2;font-size:20px;'> Outbound Calls: <strong>{outbound_call_count_x}</strong></p>", unsafe_allow_html=True)
-        st.markdown(f"<p style='color:#5cb7a2;font-size:20px;'> Inbound Calls: <strong>{inbound_call_count_x}</strong></p>", unsafe_allow_html=True)
-        st.markdown(f"<p style='color:green;font-size:20px;'> &nbsp;&nbsp;&nbsp;&nbsp;Answered calls: <strong>{Answer_Call_count_x}</strong></p>", unsafe_allow_html=True)
-        st.markdown(f"<p style='color:orange;font-size:20px;'> &nbsp;&nbsp;&nbsp;&nbsp;Missed calls: <strong>{Missed_Call_count_x}</strong></p>", unsafe_allow_html=True)
-        st.markdown(f"<p style='color:red;font-size:20px;'> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;No Follow-up: <strong>{did_not_meet_criteria_count_x}</strong></p>", unsafe_allow_html=True)
-        st.markdown(f"<p style='color:#D8D800;font-size:20px;'> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Follow-up <1 hr: <strong>{Nurse_Call_Back_count_x + Patient_Call_Back_count_x}</strong></p>", unsafe_allow_html=True)
-        st.markdown(f"<p style='color:#D8D800;font-size:18px;'> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;By Nurse: <strong>{Nurse_Call_Back_count_x}</strong></p>", unsafe_allow_html=True)
-        st.markdown(f"<p style='color:#D8D800;font-size:18px;'> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;By Patient: <strong>{Patient_Call_Back_count_x}</strong></p>", unsafe_allow_html=True)
-    
-        # Display a bar chart
-        st.subheader('Number of Calls Meeting Criteria')
-
-        # Create a Plotly Express bar chart
-        fig = px.bar(
-            x=['Answered', 'Missed','Called Back in selected time by Nurse','Called Back in selected time by Patient','Did not call back in selected time'],
-            y=[Answer_Call_count_x, Missed_Call_count_x, Nurse_Call_Back_count_x, Patient_Call_Back_count_x, did_not_meet_criteria_count_x],
-            text=[Answer_Call_count_x, Missed_Call_count_x, Nurse_Call_Back_count_x, Patient_Call_Back_count_x, did_not_meet_criteria_count_x],  # Display count values on top of the bars
-            labels={'y': 'Count', 'x': 'Criteria'},
-            color=['Answered', 'Missed','Called Back in selected time by Nurse','Called Back in selected time by Patient','Did not call back in selected time'],
-            color_discrete_map={'Answered': '#5cb7a2', 'Missed': '#74e5cb', 'Called Back in selected time by Nurse': 'yellow', 'Called Back in selected time by Patient': 'orange', 'Did not call back in selected time': 'red'}
-        )
-
-        # Update layout to display text on top of the bars
-        fig.update_traces(textposition='outside')
-
-        # Display the Plotly Express chart using Streamlit
-        st.plotly_chart(fig)
-
-        # Display the evaluated performance based on selected criteria and dates
-        st.subheader('Performance Evaluation Results')
-        if performance_criteria == 'All':
-            # Display only specific columns
-            display_columns = ['From', 'To', 'Start Time (local)', 'Duration', 'Direction', 'Performance']
-            st.write(filtered_df_x[display_columns])
-        elif performance_criteria == 'Answered':
-            # Display only specific columns
-            display_columns = ['From', 'To', 'Start Time (local)', 'Duration', 'Direction', 'Performance']
-            st.write(filtered_df_x[filtered_df_x['Performance'].str.startswith('Met criteria. Answered')][display_columns])
-        elif performance_criteria == 'Missed':
-            # Display only specific columns
-            display_columns = ['From', 'To', 'Start Time (local)', 'Duration', 'Direction', 'Performance']
-            st.write(filtered_df_x[filtered_df_x['Performance'].str.startswith('Met criteria. Did not answer.') | filtered_df_x['Performance'].str.startswith('Did not')])
-        elif performance_criteria == 'No Follow-up':
-            # Display only specific columns
-            display_columns = ['From', 'To', 'Start Time (local)', 'Duration', 'Direction', 'Performance']
-            st.write(filtered_df_x[filtered_df_x['Performance'].str.startswith('Did not')][display_columns])
-        elif performance_criteria == 'Follow-up >1hr and <10hr':
-            # Display only specific columns
-            display_columns = ['From', 'To', 'Start Time (local)', 'Duration', 'Direction', 'Performance']
-            st.write(filtered_df_x[filtered_df_x['Performance'].str.startswith('Did not Met criteria. Did not answer. Nurse called back') | filtered_df['Performance'].str.startswith('Did not Met criteria. Did not answer. Patient called back')])
-        elif performance_criteria == 'Follow-up <1hr':
-            # Display only specific columns
-            display_columns = ['From', 'To', 'Start Time (local)', 'Duration', 'Direction', 'Performance']
-            st.write(filtered_df_x[filtered_df_x['Performance'].str.startswith('Met criteria')][display_columns])
     
 if __name__ == "__main__":
     main()
